becb7b7921737efed7f64009d720950ea25d1a40
salemelkhamess <salemelkhamess@gmail.com>
2025-11-12 22:15:03 +0000

    Chage instance method

--- src/main/java/org/example/decorator/Test.java
+++ src/main/java/org/example/decorator/Test.java
@@ -1,6 +1,7 @@
 package org.example.decorator;
 
 import org.example.decorator.boissons.Boisson;
+import org.example.decorator.boissons.Deca;
 import org.example.decorator.boissons.Expresso;
 import org.example.decorator.docorateur.Caramel;
 import org.example.decorator.docorateur.Chocolat;
@@ -8,31 +9,9 @@ import org.example.decorator.docorateur.Noisette;
 
 public class Test {
     public static void main(String[] args) {
-        Boisson boisson;
-        boisson = new Expresso();
+        Boisson boisson = new Chocolat(new Noisette(new Caramel( new Chocolat(new Caramel(new Deca())))));
         System.out.println(boisson.getDescription());
         System.out.println(boisson.cout());
-        System.out.println("-------------------------");
-        boisson =new Chocolat(boisson);
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
-        System.out.println("-------------------------");
-        boisson = new Noisette(boisson);
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
-        System.out.println("-------------------------");
-        boisson = new Chocolat(boisson);
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
-        System.out.println("-------------------------");
 
-        boisson = new Caramel(boisson);
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
-        System.out.println("-------------------------");
-
-        boisson = new Chocolat(boisson);
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
     }
 }
